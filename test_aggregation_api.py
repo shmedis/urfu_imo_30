4749cf7db06d9ffcdf075c745d5ec05336ef72d1
shmedis <shmedis@gmail.com>
2023-04-23 12:30:47 +0500

    Refactoring

--- test_aggregation_api.py
+++ test_aggregation_api.py
@@ -14,9 +14,9 @@ def test_aggr():
     test_text = """Давайте попробуем более сложный пример теста, который сначала вызывает метод predict нашего API, а затем
     передает в него данные для распознавания и сравнивает полученный ответ с ожидаемым. Мы продолжим рассматривать пример
     приложения, которое определяет тональность текстов на английском языке"""
-    len_in = len(test_text)
+    len_input = len(test_text)
     response = client.post("/aggr/", json={"text": test_text})
     json_data = response.json()
-    len_out = len(json_data)
+    len_output = len(json_data)
 
-    assert len_in > len_out
+    assert len_input > len_output
